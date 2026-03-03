6a4456a8d57df584e4bf6333ec35508a221211a1
AyushMahera <ayush.mahera@aliansoftware.com>
2026-03-03 09:22:00 +0530

    Validating task form

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -12,12 +12,37 @@ const TaskForm = () => {
     return [];
   });
 
-  const [formData, setFormData] = useState({
+  const initialState = {
     title: "",
     details: "",
-  });
+  };
 
-  const [error, setError] = useState("");
+  const validationField = {
+    title: {
+      required: true,
+    },
+    details: {
+      required: true,
+    },
+  };
+
+  function validate(data) {
+    let newError = {};
+    Object.keys(validationField).forEach((field) => {
+      const rules = validationField[field];
+      const value = data[field];
+
+      if (rules.required && !value.trim()) {
+        newError[field] = "This field is required";
+        return;
+      }
+    });
+    return newError;
+  }
+
+  const [formData, setFormData] = useState(initialState);
+
+  const [error, setError] = useState(initialState);
 
   const { loggedUser } = useContext(AuthContext);
 
@@ -26,14 +51,33 @@ const TaskForm = () => {
   }, [tasks]);
 
   function handleForm(e) {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
+    const updateForm = {
+      ...formData,
+      [e.target.name]: e.target.value,
+    };
+    setFormData(updateForm);
+
+    const fieldError = validate(updateForm)[e.target.name];
+
+    if (fieldError) {
+      setError({
+        ...error,
+        [e.target.name]: fieldError,
+      });
+    } else {
+      setError({
+        ...error,
+        [e.target.name]: "",
+      });
+    }
   }
 
   function handleSubmit(e) {
     e.preventDefault();
-    if (!(formData.title.trim() && formData.details.trim())) {
-      setError("All fields required");
-      return;
+    const validateErrors = validate(formData);
+    if(Object.keys(validateErrors).length > 0){
+      setError(validateErrors);
+      return
     }
     toast.success("Task added successfully!", {
       position: "bottom-center",
@@ -57,33 +101,44 @@ const TaskForm = () => {
       },
       ...tasks,
     ]);
-    setFormData({
-      title: "",
-      details: "",
-    });
+    setFormData(initialState);
   }
 
   return (
     <div className="bg-gray-400 text-white w-100 rounded-xl p-4 flex flex-col items-center gap-5">
       <h2 className="text-3xl font-medium">Task Form</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          onChange={handleForm}
-          value={formData.title}
-          type="text"
-          name="title"
-          placeholder="Enter task title"
-          className="border p-2 w-full bg-gray-200 rounded-xl text-black focus:outline-none focus:ring-2 focus:ring-blue-400 text-xl mb-3"
-        />
-        <textarea
-          onChange={handleForm}
-          type="text"
-          value={formData.details}
-          name="details"
-          placeholder="Enter task details"
-          className="border p-2 w-full h-35 bg-gray-200 rounded-xl text-black focus:outline-none focus:ring-2 focus:ring-blue-400 text-xl mb-3 resize-none"
-        />
-        {error && <p className="text-red-600 mb-2">{error}</p>}
+      <form onSubmit={handleSubmit} className="w-full">
+        <div className="w-full mb-1">
+          <label className="text-black" htmlFor="title">
+            Title
+          </label>
+          <input
+            onChange={handleForm}
+            value={formData.title}
+            type="text"
+            id="title"
+            name="title"
+            placeholder="Enter Title"
+            className="border p-2 w-full bg-gray-200 rounded-xl text-black focus:outline-none focus:ring-2 focus:ring-blue-400 text-xl"
+          />
+          {error.title && <p className="text-red-500 text-sm mt-1">{error.title}</p>}
+        </div>
+
+        <div className="w-full mb-3">
+          <label className="text-black" htmlFor="details">
+            Details
+          </label>
+          <textarea
+            onChange={handleForm}
+            type="text"
+            value={formData.details}
+            name="details"
+            placeholder="Enter task details"
+            className="border p-2 w-full h-35 bg-gray-200 rounded-xl text-black focus:outline-none focus:ring-2 focus:ring-blue-400 text-xl resize-none"
+          />
+          {error.details && <p className="text-red-500 text-sm">{error.details}</p>}
+        </div>
+
         <button
           type="submit"
           className="bg-blue-500 w-full p-2 rounded-xl hover:bg-blue-400 transition-colors duration-200 cursor-pointer"
@@ -92,7 +147,12 @@ const TaskForm = () => {
         </button>
       </form>
       <div className="w-full flex items-center justify-end mb-2">
-        <Link to='/tasks' className="flex items-center gap-1 hover:text-blue-300 transition-colors">View Tasks <ArrowRight size={22} className="pt-1"/></Link>
+        <Link
+          to="/tasks"
+          className="flex items-center gap-1 hover:text-blue-300 transition-colors"
+        >
+          View Tasks <ArrowRight size={22} className="pt-1" />
+        </Link>
       </div>
     </div>
   );
